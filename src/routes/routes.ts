e77cef78db4853464f361b82d9932a13452d1481
Marcelo8173 <marceloandrebio@gmail.com>
2020-10-16 23:03:47 -0400

    config multer

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import OrphanagesController from '../ controller/orphanagesController';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
-routes.post('/create_orphanages', OrphanagesController.create);
+routes.post('/create_orphanages', upload.array('images') ,OrphanagesController.create);
 routes.get('/create_orphanages', OrphanagesController.index);
 routes.get('/create_orphanages/:id', OrphanagesController.show);
 
