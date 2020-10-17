12a17d92c60d74b97096909eda1782a7bac1f4cf
Marcelo8173 <marceloandrebio@gmail.com>
2020-10-17 15:01:36 -0400

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import './database/connection';
 import routes from './routes/routes';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
